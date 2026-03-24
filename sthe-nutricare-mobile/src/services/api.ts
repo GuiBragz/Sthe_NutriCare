77c9bcb15a0644dba6a5757703757b58492a0dff
Guilherme Braga ferreira <guilhermebragaferreira40@gmail.com>
2026-03-24 19:34:55 -0300

    update

--- sthe-nutricare-mobile/src/services/api.ts
+++ sthe-nutricare-mobile/src/services/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const MEU_IP = "192.168.1.6"; // Confirme se o IP ainda é esse
-
+// Adeus IP local! Agora usamos a URL do servidor na nuvem.
 const api = axios.create({
-  baseURL: `http://${MEU_IP}:3000`,
+  baseURL: "https://sthe-nutricare-api.onrender.com",
 });
 
 api.interceptors.request.use(
@@ -14,7 +13,7 @@ api.interceptors.request.use(
     if (token) {
       // O formato com colchetes é universal e não falha
       config.headers["Authorization"] = `Bearer ${token}`;
-      console.log("🎫 Enviando token para a rota:", config.url);
+      console.log("🎫 Enviando token para o Render na rota:", config.url);
     } else {
       console.log(
         "❌ Nenhum token encontrado no celular para a rota:",
